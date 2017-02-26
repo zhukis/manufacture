1a2b5c8fc93c8d15a21b5d77dd356a0c5c92834e
Ihar Zhuk <zdorovichok-zhuchok@yandex.ru>
2017-02-26 22:38:26 +0300

    Manufacture basic structure.

--- manufacture.js
+++ manufacture.js
@@ -1,3 +1,17 @@
-/**
- * Created by Игорь on 26.02.2017.
- */
+function Manufacture() {
+    this._eventHandlers = {}
+}
+
+Manufacture.prototype.setActivity = function (activityName) {
+    if (!this._eventHandlers) this._eventHandlers = {};
+
+    if (!this._eventHandlers[activityName]) {
+        this._eventHandlers[activityName] = {};
+        this._eventHandlers[activityName]["productAmount"] = 0;
+        this._eventHandlers[activityName]["actions"] = [];
+    }
+
+    for (var i = 1; i < arguments.length; i++) {
+        this._eventHandlers[activityName]["actions"].push(arguments[i]);
+    }
+};
