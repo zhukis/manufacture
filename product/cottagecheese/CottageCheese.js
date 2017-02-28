463a4ed9703af04c4217042b8234556e11b14a15
Ihar Zhuk <zdorovichok-zhuchok@yandex.ru>
2017-02-28 17:48:08 +0300

    Intermediate result of assign.

--- product/cottagecheese/CottageCheese.js
+++ product/cottagecheese/CottageCheese.js
@@ -1,7 +1,20 @@
 function CottageCheese(baseProductName, baseProductAmount, cottageCheeseAmount) {
     BaseProduct.call(this, baseProductName, baseProductAmount);
+
     this.amount = cottageCheeseAmount;
 }
+
+CottageCheese.prototype = {
+    constructor: CottageCheese,
+
+    getAmount: function () {
+        return this;
+    }
+};
+
+// var proto = Object.create(BaseProduct.prototype);
+// CottageCheese.prototype = _.assign({}, proto, CottageCheese.prototype);
+// debugger;
 
 CottageCheese.prototype = Object.create(BaseProduct.prototype);
 CottageCheese.prototype.constructor = CottageCheese;
