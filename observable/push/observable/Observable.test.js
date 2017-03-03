f5c0f7cd997c011064ef42b7d1b00bfb7bd5141b
Ihar Zhuk <zdorovichok-zhuchok@yandex.ru>
2017-03-03 18:00:30 +0300

    Temp result of testing.

--- observable/push/observable/Observable.test.js
+++ observable/push/observable/Observable.test.js
@@ -32,4 +32,45 @@ describe("A spec using for checking observable objects", function () {
 
         expect(f).toThrowError(TypeError);
     });
+
+    it("checks subscribe and unsubscribe actions", function () {
+        var observer = new Observer();
+
+        spyOn(observable, 'subscribe');
+        spyOn(observable, 'unsubscribe');
+
+        observable.subscribe(observer);
+        observable.unsubscribe(observer);
+
+        expect(observable.subscribe).toHaveBeenCalled();
+        expect(observable.subscribe).toHaveBeenCalledTimes(1);
+        expect(observable.subscribe).toHaveBeenCalledWith(jasmine.objectContaining({statistics: []}));
+        expect(observable.unsubscribe).toHaveBeenCalled();
+        expect(observable.unsubscribe).toHaveBeenCalledTimes(1);
+        expect(observable.subscribe).toHaveBeenCalledWith(jasmine.objectContaining({statistics: []}));
+    });
+
+    it("checks publishing", function () {
+        var observer = new Observer();
+
+        spyOn(observable, 'publish').and.callThrough();
+        spyOn(observable, 'unsubscribe').and.callThrough();
+        spyOn(observer, 'add').and.callThrough();
+        spyOn(observer, 'showStatistics');
+
+        observable.subscribe(observer);
+        observable.publish("test");
+        observer.showStatistics();
+        // expect(observer.statistics).toEqual(jasmine.arrayContaining(["test"]));
+        observable.unsubscribe();
+        // expect(observer.statistics).toEqual(jasmine.arrayContaining([]));
+
+        expect(observable.publish).toHaveBeenCalled();
+        expect(observable.publish).toHaveBeenCalledWith("test");
+        expect(observer.showStatistics).toHaveBeenCalled();
+        expect(observable.unsubscribe).toHaveBeenCalled();
+        // expect(observable.subscribers).toEqual(jasmine.arrayContaining([]));
+        expect(observer.add).toHaveBeenCalled();
+
+    });
 });
